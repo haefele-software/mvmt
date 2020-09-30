b417a21e8345a319d56b1082acea74ac31e9358a
askharley <harl3y.fer@gmail.com>
2020-09-30 16:50:52 +0200

    Initial commit.

---
+++ index.js
@@ -0,0 +1,13 @@
+import { useEffect, useState } from 'react';
+
+export const useEkko = (name, initialValue) => {
+  const initialStoredValue = window.localStorage.getItem(name) !== undefined ? JSON.parse(window.localStorage.getItem(name)) : initialValue;
+
+  const [value, setValue] = useState(initialStoredValue);
+
+  useEffect(() => {
+    window.localStorage.setItem(name, JSON.stringify(value));
+  }, [name, value]);
+
+  return [value, setValue];
+}
